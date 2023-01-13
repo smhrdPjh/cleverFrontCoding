44ff8665a36919956b86321669e2a8ca62201622
smhrdPjh <pjh01082037858@gmail.com>
2023-01-13 11:43:19 +0900

    출돌해결

--- src/component/CalendarInput.js
+++ src/component/CalendarInput.js
@@ -6,146 +6,164 @@ const CalendarInput = () => {
   const [PlanStartMin, setPlanStartMin] = useState(0);
   const [PlanEndHr, setPlanEndHr] = useState(0);
   const [PlanEndMin, setPlanEndMin] = useState(0);
-  const [Worker, setWorker] = useState("");
-  const [PlanYear, setPlanYear] = useState(0);
-  const [PlanMonth, setPlanMonth] = useState(0);
+  const [Worker, setWorker] = useState("직원을선택 해주세요.");
+  const [PlanYear, setPlanYear] = useState(2023);
+  const [PlanMonth, setPlanMonth] = useState(2);
   const [Day, setDay] = useState([]);
   const date = new Date();
   var year = date.getFullYear();
   var month = date.getMonth();
 
-  function getDayOfWeek(yyyy, mm) {
-    var arrMonthLength = [];
-    //특정월의 마지막날        년, 월
+  const getDayOfWeek = (yyyy, mm, arrChoiceDay) => {
+    console.log("요일", [arrChoiceDay]);
+    //특정date1의 마지막날        년, date1
     let lastDate = new Date(yyyy, mm, 0).getDate();
-    // console.log("마지막날", lastDate);
 
-    var 일;
-    var 월;
-    var 화;
-    var 수;
-    var 목;
-    var 금;
-    var 토;
-    var 일1 = [];
-    var 월1 = [];
-    var 화1 = [];
-    var 수1 = [];
-    var 목1 = [];
-    var 금1 = [];
-    var 토1 = [];
+    var date0;
+    var date1;
+    var date2;
+    var date3;
+    var date4;
+    var date5;
+    var date6;
+    var arrDate0 = [];
+    var arrDate1 = [];
+    var arrDate2 = [];
+    var arrDate3 = [];
+    var arrDate4 = [];
+    var arrDate5 = [];
+    var arrDate6 = [];
 
     const week = [0, 1, 2, 3, 4, 5, 6];
 
     var stringYear = String(yyyy);
     var stringMonth = String(mm);
-    //특정일자 첫날 요일
+
     const dayOfWeek =
       week[new Date(stringYear + "-" + stringMonth + "-" + "1").getDay()];
-    // console.log("요일",dayOfWeek);
 
     if (dayOfWeek === 0) {
-      일 = 1;
-      월 = 2;
-      화 = 3;
-      수 = 4;
-      목 = 5;
-      금 = 6;
-      토 = 7;
+      date0 = 1;
+      date1 = 2;
+      date2 = 3;
+      date3 = 4;
+      date4 = 5;
+      date5 = 6;
+      date6 = 7;
     } else if (dayOfWeek === 1) {
-      월 = 1;
-      화 = 2;
-      수 = 3;
-      목 = 4;
-      금 = 5;
-      토 = 6;
-      일 = 7;
+      date1 = 1;
+      date2 = 2;
+      date3 = 3;
+      date4 = 4;
+      date5 = 5;
+      date6 = 6;
+      date0 = 7;
     } else if (dayOfWeek === 2) {
-      화 = 1;
-      수 = 2;
-      목 = 3;
-      금 = 4;
-      토 = 5;
-      일 = 6;
-      월 = 7;
+      date2 = 1;
+      date3 = 2;
+      date4 = 3;
+      date5 = 4;
+      date6 = 5;
+      date0 = 6;
+      date1 = 7;
     } else if (dayOfWeek === 3) {
-      수 = 1;
-      목 = 2;
-      금 = 3;
-      토 = 4;
-      일 = 5;
-      월 = 6;
-      화 = 7;
+      date3 = 1;
+      date4 = 2;
+      date5 = 3;
+      date6 = 4;
+      date0 = 5;
+      date1 = 6;
+      date2 = 7;
     } else if (dayOfWeek === 4) {
-      목 = 1;
-      금 = 2;
-      토 = 3;
-      일 = 4;
-      월 = 5;
-      화 = 6;
-      수 = 7;
+      date4 = 1;
+      date5 = 2;
+      date6 = 3;
+      date0 = 4;
+      date1 = 5;
+      date2 = 6;
+      date3 = 7;
     } else if (dayOfWeek === 5) {
-      금 = 1;
-      토 = 2;
-      일 = 3;
-      월 = 4;
-      화 = 5;
-      수 = 6;
-      목 = 7;
+      date5 = 1;
+      date6 = 2;
+      date0 = 3;
+      date1 = 4;
+      date2 = 5;
+      date3 = 6;
+      date4 = 7;
     } else if (dayOfWeek === 6) {
-      토 = 1;
-      일 = 2;
-      월 = 3;
-      화 = 4;
-      수 = 5;
-      목 = 6;
-      금 = 7;
+      date6 = 1;
+      date0 = 2;
+      date1 = 3;
+      date2 = 4;
+      date3 = 5;
+      date4 = 6;
+      date5 = 7;
     }
 
     for (let i = 0; i < lastDate; i += 7) {
-      일1.push(일 + i);
-      월1.push(월 + i);
-      화1.push(화 + i);
-      수1.push(수 + i);
-      목1.push(목 + i);
-      금1.push(금 + i);
-      토1.push(토 + i);
+      arrDate0.push(date0 + i);
+      arrDate1.push(date1 + i);
+      arrDate2.push(date2 + i);
+      arrDate3.push(date3 + i);
+      arrDate4.push(date4 + i);
+      arrDate5.push(date5 + i);
+      arrDate6.push(date6 + i);
     }
 
-    if (일1[일1.length - 1] > lastDate) {
-      일1.splice(일1.length - 1);
+    if (arrDate0[arrDate0.length - 1] > lastDate) {
+      arrDate0.splice(arrDate0.length - 1);
     }
 
-    if (월1[월1.length - 1] > lastDate) {
-      월1.splice(월1.length - 1);
+    if (arrDate1[arrDate1.length - 1] > lastDate) {
+      arrDate1.splice(arrDate1.length - 1);
     }
-    if (화1[화1.length - 1] > lastDate) {
-      화1.splice(화1.length - 1);
+    if (arrDate2[arrDate2.length - 1] > lastDate) {
+      arrDate2.splice(arrDate2.length - 1);
     }
-    if (수1[수1.length - 1] > lastDate) {
-      수1.splice(수1.length - 1);
+    if (arrDate3[arrDate3.length - 1] > lastDate) {
+      arrDate3.splice(arrDate3.length - 1);
     }
-    if (목1[목1.length - 1] > lastDate) {
-      목1.splice(목1.length - 1);
+    if (arrDate4[arrDate4.length - 1] > lastDate) {
+      arrDate4.splice(arrDate4.length - 1);
     }
-    if (금1[금1.length - 1] > lastDate) {
-      금1.splice(금1.length - 1);
+    if (arrDate5[arrDate5.length - 1] > lastDate) {
+      arrDate5.splice(arrDate5.length - 1);
     }
-    if (토1[토1.length - 1] > lastDate) {
-      토1.splice(토1.length - 1);
+    if (arrDate6[arrDate6.length - 1] > lastDate) {
+      arrDate6.splice(arrDate6.length - 1);
     }
-    // console.log("일요일",일1)
-    // console.log("월요일",월1)
-    // console.log("화요일",화1)
-    // console.log("수요일",수1)
-    // console.log("목요일",목1)
-    // console.log("금요일",금1)
-    // console.log("토요일",토1)
+
+    var sequenceDay = [
+      arrDate0,
+      arrDate1,
+      arrDate2,
+      arrDate3,
+      arrDate4,
+      arrDate5,
+      arrDate6,
+    ];
+    var selectedDate = [];
+    for (var i = 0; i < arrChoiceDay.length; i++) {
+      for (var j = 0; j < 7; j++) {
+        if (Number(arrChoiceDay[i]) === j) {
+          for (var k = 0; k < sequenceDay[j].length; k++) {
+            selectedDate.push(sequenceDay[j][k]);
+          }
+        }
+      }
+    }
+
+    console.log("최종날짜", selectedDate);
+
+    console.log("일요일", arrDate0);
+    console.log("월요일", arrDate1);
+    console.log("화요일", arrDate2);
+    console.log("수요일", arrDate3);
+    console.log("목요일", arrDate4);
+    console.log("금요일", arrDate5);
+    console.log("토요일", arrDate6);
     return dayOfWeek;
-  }
-  useEffect(() => {
-    getDayOfWeek(2022, 11);
-  }, []);
+  };
 
   //임시용 나중에 back에서 받아올예정
   const workerList = [
@@ -208,7 +226,7 @@ const CalendarInput = () => {
     setWorker(e.target.value);
   };
 
-  // 선택한 요일값을 배열에 넣기
+  // 선택한 요일값 setDay하기
   const day = (e) => {
     if (e.target.checked && !checkOn.includes(e.target.value)) {
       checkOn.push(e.target.value);
@@ -335,13 +353,15 @@ const CalendarInput = () => {
             <br />
             퇴근 분: {PlanEndMin}
             <br />
-            요일 : {checkOn}
+            요일 : {Day}
             <br />년 : {PlanYear}
             <br />월 : {PlanMonth}
           </p>
         </tr>
 
-        <button>등록하기</button>
+        <button onClick={getDayOfWeek(PlanYear, PlanMonth, Day)}>
+          등록하기
+        </button>
       </form>
     </div>
   );
