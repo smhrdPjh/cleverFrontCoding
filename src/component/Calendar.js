d06e4990d1db1a8d0547282906f72dadfc2060d6
smhrdPjh <pjh01082037858@gmail.com>
2023-01-17 17:21:14 +0900

    이제다음 수정요청 작업 시작

--- src/component/Calendar.js
+++ src/component/Calendar.js
@@ -5,27 +5,28 @@ import "../style/calendar.css";
 import googleCalendar from "@fullcalendar/google-calendar";
 import interaction from "@fullcalendar/interaction";
 import { v4 as uuidv4 } from "uuid";
-var thisDayList = [
-  {
-    workerName: "박진형",
-    startTime: "16:10",
-    endTime: "21:10",
-  },
-  {
-    workerName: "박형주",
-    startTime: "09:10",
-    endTime: "16:10",
-  },
-  {
-    workerName: "나소연",
-    startTime: "21:10",
-    endTime: "04:10",
-  },
-];
-const Calendar = () => {
-  const [thisDayListState, setThisDayListState] = useState([thisDayList]);
-  const [arrAddListState, setArrAddListState] = useState([]);
 
+const Calendar = () => {
+  var thisDayList = useRef([
+    {
+      workerName: "박진형",
+      startTime: "16:10",
+      endTime: "21:10",
+    },
+    {
+      workerName: "박형주",
+      startTime: "09:10",
+      endTime: "16:10",
+    },
+    {
+      workerName: "나소연",
+      startTime: "21:10",
+      endTime: "04:10",
+    },
+  ]);
+
+  const [thisDayListState, setThisDayListState] = useState([]);
+  const [arrAddListState, setArrAddListState] = useState([]);
   const apiKey = "AIzaSyAHG8iIVB4i-q5o7KRjdvKcwVc67JzZEWc";
   const date = new Date();
   const year = date.getFullYear();
@@ -33,11 +34,9 @@ const Calendar = () => {
   const day = date.getDate();
   const today = year + "-" + month + 1 + "-" + day;
   let arrAddList = useRef([]);
-  const [addCount, setAddCount] = useState(0);
   const [selectedDate, setSelectecDate] = useState(today);
   const workingTime = [{ arrive: "07:00", live: "18:00" }];
   console.log("리스트상태", arrAddList.current);
-  var countNumRef = useRef(0);
   const setSchedul = [
     { title: "07:00~15:00", date: "2023-01-01" },
     { title: "07:00~15:00", date: "2023-01-02" },
@@ -75,12 +74,12 @@ const Calendar = () => {
   };
 
   const planModification = () => {
-    var result = thisDayList.map((item, index) => {
+    var result = thisDayList.current.map((item, index) => {
       return (
         <tr key={`${item.workerName}${index}`}>
           <select
             onChange={(e) => {
-              thisDayList[index].workerName = e.target.value;
+              thisDayList.current[index].workerName = e.target.value;
             }}
             defaultValue={item.workerName}
           >
@@ -90,7 +89,7 @@ const Calendar = () => {
             type="time"
             defaultValue={item.startTime}
             onChange={(e) => {
-              thisDayList[index].startTime = e.target.value;
+              thisDayList.current[index].startTime = e.target.value;
             }}
           />
           ~
@@ -98,16 +97,14 @@ const Calendar = () => {
             type="time"
             defaultValue={item.endTime}
             onChange={(e) => {
-              thisDayList[index].endTime = e.target.value;
-              console.log("투데이배열", thisDayList);
+              thisDayList.current[index].endTime = e.target.value;
+              console.log("투데이배열", thisDayList.current);
             }}
           />
           <button
             onClick={() => {
-              thisDayList.splice(index, 1);
-              console.log("삭제" + index);
-              console.log("삭제후 리스트", thisDayList);
-              setThisDayListState([...thisDayList]);
+              thisDayList.current.splice(index, 1);
+              setThisDayListState([thisDayList.current]);
             }}
           >
             삭제
@@ -120,7 +117,6 @@ const Calendar = () => {
   };
 
   const addModification = () => {
-    console.log("함수안에서 상태", arrAddList.current);
     let result = arrAddList.current.map((item, index) => {
       return (
         <tr key={uuidv4()}>
@@ -170,7 +166,7 @@ const Calendar = () => {
   };
 
   const submitModification = () => {
-    console.log("최종배열", thisDayList.concat(arrAddList.current));
+    console.log("최종배열", thisDayList.current.concat(arrAddList.current));
   };
   console.log("상태 :", arrAddListState);
   return (
