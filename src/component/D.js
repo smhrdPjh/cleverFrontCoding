d770ea29a941fdcd4abea4d5515e06a64e5e5344
smhrdPjh <pjh01082037858@gmail.com>
2023-01-21 02:24:12 +0900

    새벽커밋

--- src/component/D.js
+++ src/component/D.js
@@ -1,7 +1,42 @@
-import React from "react";
+  const planModification = () => {
+    var result = selectedWorkerList.current.map((item, index) => {
+      return (
+        <tr key={`${item.workerName}${index}`}>
+          <select
+            onChange={(e) => {
+              selectedWorkerList.current[index].workerName = e.target.value;
+            }}
+            defaultValue={item.workerName}
+          >
+            {workerListRendering()}
+          </select>
+          <input
+            type="time"
+            defaultValue={item.startTime}
+            onChange={(e) => {
+              selectedWorkerList.current[index].startTime = e.target.value;
+            }}
+          />
+          ~
+          <input
+            type="time"
+            defaultValue={item.endTime}
+            onChange={(e) => {
+              selectedWorkerList.current[index].endTime = e.target.value;
+              console.log("투데이배열", selectedWorkerList.current);
+            }}
+          />
+          <button
+            onClick={() => {
+              selectedWorkerList.current.splice(index, 1);
+              setThisDayListState([selectedWorkerList.current]);
+            }}
+          >
+            삭제
+          </button>
+        </tr>
+      );
+    });
 
-const D = () => {
-  return <div>D</div>;
-};
-
-export default D;
+    return result;
+  };
